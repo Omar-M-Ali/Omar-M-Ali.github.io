4c82b1fe88daeaa0314aa5ce8fa3f5382c57e40c
Omar M. Ali <mvac2012cg>
2016-01-26 05:45:24 +0200

    update

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 
-	//
+	$('.sora1').click(function() {
+		$('.limage').html("<img src='/assets/img/asterBIG.jpg'>")
+		$('#lightbox').fadeToggle(500);
+	});
+
+	$('.sora2').click(function() {
+		$('.limage').html("<img src='/assets/img/robosBIG.jpg'>")
+		$('#lightbox').fadeToggle(500);
+	});
+
+	$('.sora3').click(function() {
+		$('.limage').html("<img src='/assets/img/wiresBIG.jpg'>")
+		$('#lightbox').fadeToggle(500);
+	});
+
+	$('#lightbox').click(function() {
+		$('#lightbox').fadeToggle(500);
+	});
 
 });
