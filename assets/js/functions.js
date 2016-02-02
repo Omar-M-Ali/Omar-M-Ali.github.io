8c3a6f4c4341c1410bcee6d35bb303100a9f92e6
Omar M. Ali <mvac2012cg>
2016-02-02 09:13:29 +0200

    update

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,60 +1,77 @@
 $(document).ready(function() {
 
 	$('.swap1').click(function() {
-		$('.limage').html("<iframe src='https://player.vimeo.com/video/29364234?color=f0050a' width='1280' height='720' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>");
+		// $('.limage').html("<iframe src='https://player.vimeo.com/video/29364234?color=f0050a' width='1280' height='720' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>");
+		$('.limage').html("<img src='/assets/img/1.jpg'>");
 		$('#lightbox').fadeToggle(400);
 	});
 
-	$(".swap1").hover(function() {
-	    $(this).find('.sora1').toggle(0);
-	    $(this).find('.soraBW').toggle(0);
+	$(".work-item.swap1").hover(function() {
+	    $(this).find('.BW1').toggle(0);
+	    $(this).find('.clr').toggle(0);
 	}, function() {
-	    $(this).find('.sora1').toggle(0);
-	    $(this).find('.soraBW').toggle(0);
-		}
-	);
-
-	$('.swap4').click(function() {
-		$('.limage').html("<img src='/assets/img/robosBIG.jpg'>");
-		$('#lightbox').fadeToggle(400);
-	});
-
-	$(".swap4").hover(function() {
-	    $(this).find('.sora4').toggle(0);
-	    $(this).find('.soraBW').toggle(0);
-	}, function() {
-	    $(this).find('.sora4').toggle(0);
-	    $(this).find('.soraBW').toggle(0);
-		}
-	);
-
-	$('.swap3').click(function() {
-		$('.limage').html("<img src='/assets/img/wiresBIG.jpg'>");
-		$('#lightbox').fadeToggle(400);
-	});
-
-	$(".swap3").hover(function() {
-	    $(this).find('.sora3').toggle(0);
-	    $(this).find('.soraBW').toggle(0);
-	}, function() {
-	    $(this).find('.sora3').toggle(0);
-	    $(this).find('.soraBW').toggle(0);
+	    $(this).find('.BW1').toggle(0);
+	    $(this).find('.clr').toggle(0);
 		}
 	);
 
 	$('.swap2').click(function() {
-		$('.limage').html("<img src='/assets/img/lokalBIG.jpg'>");
+		$('.limage').html("<img src='/assets/img/2.jpg'>");
 		$('#lightbox').fadeToggle(400);
 	});
 
-	$(".swap2").hover(function() {
-	    $(this).find('.sora2').toggle(0);
-	    $(this).find('.soraBW').toggle(0);
+	$(".work-item.swap2").hover(function() {
+	    $(this).find('.BW2').toggle(0);
+	    $(this).find('.clr').toggle(0);
 	}, function() {
-	    $(this).find('.sora2').toggle(0);
-	    $(this).find('.soraBW').toggle(0);
+	    $(this).find('.BW2').toggle(0);
+	    $(this).find('.clr').toggle(0);
 		}
 	);
+
+	$('.swap3').click(function() {
+		$('.limage').html("<img src='/assets/img/3.jpg'>");
+		$('#lightbox').fadeToggle(400);
+	});
+
+	$(".work-item.swap3").hover(function() {
+	    $(this).find('.BW3').toggle(0);
+	    $(this).find('.clr').toggle(0);
+	}, function() {
+	    $(this).find('.BW3').toggle(0);
+	    $(this).find('.clr').toggle(0);
+		}
+	);
+
+	$('.swap4').click(function() {
+		$('.limage').html("<img src='/assets/img/4.jpg'>");
+		$('#lightbox').fadeToggle(400);
+	});
+
+	$(".work-item.swap4").hover(function() {
+	    $(this).find('.BW4').toggle(0);
+	    $(this).find('.clr').toggle(0);
+	}, function() {
+	    $(this).find('.BW4').toggle(0);
+	    $(this).find('.clr').toggle(0);
+		}
+	);
+
+	$('.swap12').click(function() {
+		$('.limage').html("<img src='/assets/img/12.jpg'>");
+		$('#lightbox').fadeToggle(400);
+	});
+
+	$(".work-item.swap12").hover(function() {
+	    $(this).find('.BW12').toggle(0);
+	    $(this).find('.clr').toggle(0);
+	}, function() {
+	    $(this).find('.BW12').toggle(0);
+	    $(this).find('.clr').toggle(0);
+		}
+	);
+
+
 
 	$('#lightbox').click(function() {
 		$('#lightbox').fadeToggle(400);
